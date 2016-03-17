12f582ca12ce4d0b5794dcdeacc078676c37673a
Patryk Gwiazdowski <pgwiazdowski@gmail.com>
2016-03-17 16:21:01 +0100

    edded test

--- app/src/main/java/com/blstream/neverendingstory/ServiceManager/ServiceThread.java
+++ app/src/main/java/com/blstream/neverendingstory/ServiceManager/ServiceThread.java
@@ -12,7 +12,6 @@ public class ServiceThread implements Runnable {
     public ServiceThread(long duration, TimeHolder timeHolder) {
         this.duration = duration;
         this.timeHolder = timeHolder;
-
     }
 
     /**
