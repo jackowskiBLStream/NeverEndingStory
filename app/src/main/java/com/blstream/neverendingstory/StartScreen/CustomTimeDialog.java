11a1d7077fdd42eaca088184f802a3b60af57867
jackowskiBLStream <damian.jackowski@blstream.com>
2016-03-18 12:29:26 +0100

    Spinner improved

--- app/src/main/java/com/blstream/neverendingstory/StartScreen/CustomTimeDialog.java
+++ app/src/main/java/com/blstream/neverendingstory/StartScreen/CustomTimeDialog.java
@@ -5,7 +5,9 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.blstream.neverendingstory.R;
 
@@ -13,14 +15,50 @@ import com.blstream.neverendingstory.R;
  * Created by Damian on 3/17/2016.
  */
 public class CustomTimeDialog extends DialogFragment {
+    private Button button;
     private EditText editText;
+    private IDialogHelperListener iDialogHelperListener;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.custom_time_dialog_layout, container);
+        button = (Button) view.findViewById(R.id.okButton);
         editText = (EditText) view.findViewById(R.id.timeEditText);
         getDialog().setTitle("Hello");
 
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                   float seconds = Float.parseFloat(String.valueOf(editText.getText()));
+                    if (isNumberIntegerAndGreaterThanZero(seconds)) {
+                        iDialogHelperListener.onClickOkButtonListener(seconds);
+                    } else {
+                        Toast.makeText(getContext(), "Liczba musi byc calkowita i dodatnia!", Toast.LENGTH_SHORT).show();
+                    }
+
+
+
+            }
+        });
         return view;
+    }
+
+    public void validateNumber() {
+
+    }
+
+    public boolean isNumberIntegerAndGreaterThanZero(float seconds) {
+        return seconds == (int) seconds && seconds > 0;
+    }
+
+    public boolean isNumberProperlySeparatedByDot(String textFromEditText) {
+        String pattern = "([-+]?[0-9]*/.?[0-9]+)";
+        return textFromEditText.matches(pattern);
+    }
+
+    public void setiDialogHelperListener(IDialogHelperListener iDialogHelperListener) {
+        this.iDialogHelperListener = iDialogHelperListener;
     }
 }
--- app/src/main/java/com/blstream/neverendingstory/StartScreen/StartScreenFragment.java
+++ app/src/main/java/com/blstream/neverendingstory/StartScreen/StartScreenFragment.java
@@ -6,30 +6,64 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.blstream.neverendingstory.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Damian on 3/17/2016.
  */
-public class StartScreenFragment extends Fragment  {
+
+public class StartScreenFragment extends Fragment {
     private Spinner spinner;
     private View view;
+    private CustomTimeDialog customTimeDialog;
+    private ArrayAdapter<String> adapter;
+
     @Nullable
     @Override
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.start_screen_fragment_layout, container, false);
-        addListenerOnSpinnerItemSelection();
+        manageSpinner();
         return view;
     }
 
 
+    public void manageSpinner() {
+        final List<String> spinnerArray = new ArrayList<>();
+        CustomOnItemSelectedListener customOnItemSelectedListener;
 
-    public void addListenerOnSpinnerItemSelection() {
-        spinner = (Spinner)view.findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener(getFragmentManager()));
+        spinnerArray.add("10 sekund");
+        spinnerArray.add("15 sekund");
+        spinnerArray.add("20 sekund");
+        spinnerArray.add("25 sekund");
+        spinnerArray.add("35 sekund");
+        spinnerArray.add("Zdefiiniuj wlasny czas....");
+
+
+        customOnItemSelectedListener = new CustomOnItemSelectedListener(getFragmentManager(), getContext(), spinnerArray);
+
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(customOnItemSelectedListener);
+
+        customOnItemSelectedListener.setiSpinnerHelperListener(new ISpinnerHelperListener() {
+            @Override
+            public void onAddElementToSpinnerListener() {
+                adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, spinnerArray);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+                spinner.setSelection(spinnerArray.size() - 2);
+
+            }
+        });
+        adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, spinnerArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
 }
