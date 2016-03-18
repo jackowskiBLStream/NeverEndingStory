911d2833bbfec5956e2221814c2576a6971696ec
Patryk Gwiazdowski <pgwiazdowski@gmail.com>
2016-03-18 10:14:32 +0100

    gradle update

---
+++ app/src/androidTest/java/com/blstream/neverendingstory/ServiceManager/ServiceTest.java
@@ -0,0 +1,16 @@
+package com.blstream.neverendingstory.ServiceManager;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Patryk Gwiazdowski on 18.03.2016.
+ */
+public class ServiceTest {
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+}
