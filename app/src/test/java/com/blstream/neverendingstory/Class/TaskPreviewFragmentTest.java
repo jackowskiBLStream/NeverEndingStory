5864eeb66bc9d157d79f8e037cfa2c79089cfdfe
Krzysztof Antczak <antek116@gmail.com>
2016-03-18 10:17:48 -0700

    Test for TaskPreviewFragment class

---
+++ app/src/test/java/com/blstream/neverendingstory/Class/TaskPreviewFragmentTest.java
@@ -0,0 +1,11 @@
+package com.blstream.neverendingstory.Class;
+import junit.framework.TestCase;
+
+/**
+ *
+ * Created by Krzysztof Antczak on 18.03.2016.
+ */
+public class TaskPreviewFragmentTest extends TestCase {
+
+
+}
